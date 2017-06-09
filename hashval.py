acfdf7b82f920ff900a2bb930ff5ffe70f842325
coywolf <lily.sturmann@gmail.com>
2017-06-09 10:27:40 -0400

    fixed hashval

--- hashval.py
+++ hashval.py
@@ -5,6 +5,7 @@ from simple_dag import *
 # ppath pushed from parent to child in each 
 # node object
 #
+
 def all_paths(trace):
     all_paths = []
     thegraph = dag(trace)
@@ -14,6 +15,8 @@ def all_paths(trace):
             if node.children == []:
                 all_paths.append(node.ppath)
     all_paths = "||".join(["->".join(x) for x in all_paths])
+    #print "all paths from the allpaths fxn: "
+    #print all_paths
     return all_paths 
 
 
@@ -22,6 +25,9 @@ def all_paths(trace):
 # all_paths function rather than DFT
 #
 def hashval(trace):
-    paths = all_paths(trace)
-    hashval = "".join(re.findall(r'(\d)\.1', paths))
+    trace.allpaths = all_paths(trace)
+    #print "paths: " 
+    #print trace.allpaths
+    #paths = '2586230574222222.1->2586230574555555.1->2586230574111111.1->2586230574888888.1||2586230574222222.1->2586230574555555.1->2586230574111111.1->2586230574999999.1||2586230574222222.1->2586230574555555.1->2586230574666666.1'
+    hashval = "".join(re.findall(r'(\d)\.1', trace.allpaths))
     return hashval
