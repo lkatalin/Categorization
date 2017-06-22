dea5d74a5e674d21bfff4c4bb028169bac1e23f6
coywolf <lily.sturmann@gmail.com>
2017-06-22 16:16:07 -0400

    basic edge list functionality is present

--- json_parser02.py
+++ json_parser02.py
@@ -8,20 +8,21 @@ def json_parser(file):
     Each Json file will be parsed into one trace.
     """
     with open(file, 'r') as data_file:
-        
+        edge_list = []       
+ 
         # make a dict
         json_data = json.load(data_file)
 
-#        def trace_parser(data):
-#            for key, value in data.iteritems():
-#                if key == "children" and len(value) != 0:
-#                    node_parser(value)
-#
         def parser(data):
+            keyname = data["info"]["name"]
 	    for key, value in data.iteritems():
-		print key
+		#print key
+                print "key name: " + keyname + "key: " + key
 		if key == "children" and len(value) != 0:
                     for v in value:
+                        valname = v["info"]["name"]
+                        print "    val name :" + valname
+                        edge_list.append((keyname, valname))
                         parser(v)
 
     
@@ -29,6 +30,9 @@ def json_parser(file):
  
     for node in actual_nodes:
         parser(node)
+
+    print "edges: "
+    print edge_list
 
 if __name__ == '__main__':
     json_parser("traces/rightfile.json")
