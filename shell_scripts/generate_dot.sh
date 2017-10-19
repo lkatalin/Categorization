82dabafbc03bfded37f19c13f4f3b1b2ddc849c4
coywolf <lily.sturmann@gmail.com>
2017-10-19 12:34:41 -0400

    edit shell script to use old repo

--- shell_scripts/generate_dot.sh
+++ shell_scripts/generate_dot.sh
@@ -1,4 +1,4 @@
 for f in $(find . -name '*.json')
 do
-  python /mnt/traces/tracing/structural_sort/json_dag.py $f to-file
+  python /mnt/traces/tracing/Categorization/new_json_dag.py $f to-file
 done
